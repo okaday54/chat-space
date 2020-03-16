2f0f90e5c8882472dcddbf3b324eea54c2d41458
okaday54 <okaday2020@gmail.com>
2020-03-16 17:49:38 +0900

    update

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -55,6 +55,7 @@ $(function(){
     return html;
   };
   $('.new_message').on('submit', function(e){
+    console.log("check");
     e.preventDefault();
     var formData = new FormData(this);
     var url = $(this).attr('action');
@@ -81,6 +82,7 @@ $(function(){
   });
   var reloadMessages = function() {
     var last_message_id = $('.message:last').data("message-id");
+    console.log(last_message_id);
     $.ajax({
       url: "api/messages",
       type: 'get',
